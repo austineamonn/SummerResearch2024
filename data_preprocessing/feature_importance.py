created new calculating tradeoff section. created privacy columns csv file. god the average feature importance across RNN models. Fixed numpy error (Tensorflow needs Numpy 1.26 not Numpy 2.0. Updated readme.

data_preprocessing/feature_importance.py
@@ -25,8 +25,8 @@ class FeatureImportanceAnalyzer:
         self.imputer = SimpleImputer(strategy='mean')
         self.model_career = RandomForestRegressor(random_state=42)
         self.model_future = RandomForestRegressor(random_state=42)
-        self.shap_values_career = None
-        self.shap_values_future = None
+        self.feature_importances_career = None
+        self.feature_importances_future = None
 
     def impute_data(self):
         self.features_imputed = self.imputer.fit_transform(self.features)
@@ -39,37 +39,12 @@ class FeatureImportanceAnalyzer:
         
         self.model_career.fit(X_train_career, y_train_career)
         self.model_future.fit(X_train_future, y_train_future)
+
+        self.feature_importances_career = self.model_career.feature_importances_
+        self.feature_importances_future = self.model_future.feature_importances_
         
-        """explainer_career = shap.Explainer(self.model_career, X_train_career)
-        self.shap_values_career = explainer_career(X_test_career)
-
-        explainer_future = shap.Explainer(self.model_future, X_train_future)
-        self.shap_values_future = explainer_future(X_test_future)"""
-
-    """def save_shap_summary_plot(self, shap_values, features, feature_names, target, filename):
-        plt.figure(figsize=(10, 8))  # Increase the figure size
-        shap.summary_plot(shap_values, features, feature_names=feature_names, plot_type="bar", show=False)
-        plt.title(f'SHAP Summary Plot for {target}')
-        plt.subplots_adjust(left=0.3, right=0.9, top=0.9)  # Adjust the layout to prevent cutting off feature names and title
-        plt.savefig(filename)
-        plt.close()"""
-
-    """def save_all_shap_plots(self):
-        X_train_career, X_test_career, y_train_career, y_test_career = train_test_split(
-            self.features_imputed, self.target_career, test_size=0.2, random_state=42)
-        X_train_future, X_test_future, y_train_future, y_test_future = train_test_split(
-            self.features_imputed, self.target_future, test_size=0.2, random_state=42)
-
-        career_path = self.dir_path + '/shap_summary_career_aspirations.png'
-        self.save_shap_summary_plot(self.shap_values_career, X_test_career, self.feature_names,
-                                    'Career Aspirations', career_path)
-        future_topics_path = self.dir_path + '/shap_summary_future_topics.png'
-        self.save_shap_summary_plot(self.shap_values_future, X_test_future, self.feature_names,
-                                    'Future Topics', future_topics_path)"""
-        
-    def save_feature_importance_plot(self, model, feature_names, target, filename):
+    def save_feature_importance_plot(self, importances, feature_names, target, filename):
         plt.figure(figsize=(10, 8))
-        importances = model.feature_importances_
         indices = np.argsort(importances)[::-1]
         plt.title(f'Feature Importances for {target}')
         plt.bar(range(len(importances)), importances[indices], align='center')
@@ -80,9 +55,9 @@ class FeatureImportanceAnalyzer:
 
     def save_all_feature_importance_plots(self):
         career_path = self.dir_path + '/feature_importance_career_aspirations.png'
-        self.save_feature_importance_plot(self.model_career, self.feature_names, "career aspirations", career_path)
+        self.save_feature_importance_plot(self.feature_importances_career, self.feature_names, "career aspirations", career_path)
         future_topics_path = self.dir_path + '/feature_importance_future_topics.png'
-        self.save_feature_importance_plot(self.model_future, self.feature_names, "future topics", future_topics_path)
+        self.save_feature_importance_plot(self.feature_importances_future, self.feature_names, "future topics", future_topics_path)
         
     def calculate_feature_importance(self):
         self.impute_data()
@@ -95,34 +70,50 @@ if __name__ == "__main__":
     from config import load_config
 
     # Load configuration and data
-    config = load_config()\
-    
-    # GRU1
-    data1 = pd.read_csv('/Users/austinnicolas/Documents/SummerREU2024/SummerResearch2024/data_preprocessing/RNN_models/GRU1.csv')
+    config = load_config()
 
-    analyzer1 = FeatureImportanceAnalyzer(config, data1, 'GRU1')
-    analyzer1.calculate_feature_importance()
+    # File paths for the RNN model data
+    file_paths = [
+        '/Users/austinnicolas/Documents/SummerREU2024/SummerResearch2024/data_preprocessing/RNN_models/GRU1.csv',
+        '/Users/austinnicolas/Documents/SummerREU2024/SummerResearch2024/data_preprocessing/RNN_models/LSTM1.csv',
+        '/Users/austinnicolas/Documents/SummerREU2024/SummerResearch2024/data_preprocessing/RNN_models/LSTM2.csv',
+        '/Users/austinnicolas/Documents/SummerREU2024/SummerResearch2024/data_preprocessing/RNN_models/Simple1.csv',
+        '/Users/austinnicolas/Documents/SummerREU2024/SummerResearch2024/data_preprocessing/RNN_models/Simple2.csv'
+    ]
 
-    # LSTM1
-    data2 = pd.read_csv('/Users/austinnicolas/Documents/SummerREU2024/SummerResearch2024/data_preprocessing/RNN_models/LSTM1.csv')
+    career_importances = []
+    future_importances = []
 
-    analyzer2 = FeatureImportanceAnalyzer(config, data2, 'LSTM1')
-    analyzer2.calculate_feature_importance()
+    for path in file_paths:
+        data = pd.read_csv(path)
+        analyzer = FeatureImportanceAnalyzer(config, data, os.path.basename(path).split('.')[0])
+        analyzer.calculate_feature_importance()
+        career_importances.append(analyzer.feature_importances_career)
+        future_importances.append(analyzer.feature_importances_future)
 
-    # LSTM2
-    data3 = pd.read_csv('/Users/austinnicolas/Documents/SummerREU2024/SummerResearch2024/data_preprocessing/RNN_models/LSTM2.csv')
+    # Compute average feature importances
+    avg_career_importances = np.mean(career_importances, axis=0)
+    avg_future_importances = np.mean(future_importances, axis=0)
 
-    analyzer3 = FeatureImportanceAnalyzer(config, data3, 'LSTM2')
-    analyzer3.calculate_feature_importance()
+    # Create DataFrame for average importances
+    feature_names = config["privacy"]["X_list"]
+    avg_importance_data = {
+        'Feature': feature_names,
+        'Career Aspirations Importance': avg_career_importances,
+        'Future Topics Importance': avg_future_importances
+    }
+    avg_importance_df = pd.DataFrame(avg_importance_data)
 
-    # Simple1
-    data4 = pd.read_csv('/Users/austinnicolas/Documents/SummerREU2024/SummerResearch2024/data_preprocessing/RNN_models/Simple1.csv')
+    # Plot average importances
+    fig, ax = plt.subplots(figsize=(12, 8))
 
-    analyzer4 = FeatureImportanceAnalyzer(config, data4, 'Simple1')
-    analyzer4.calculate_feature_importance()
+    avg_importance_df.plot(kind='bar', x='Feature', ax=ax)
 
-    # Simple2
-    data5 = pd.read_csv('/Users/austinnicolas/Documents/SummerREU2024/SummerResearch2024/data_preprocessing/RNN_models/Simple2.csv')
+    plt.title('Average Feature Importances for Career Aspirations and Future Topics')
+    plt.ylabel('Importance')
+    plt.xticks(rotation=45, ha='right')
+    plt.tight_layout()
 
-    analyzer5 = FeatureImportanceAnalyzer(config, data5, 'Simple2')
-    analyzer5.calculate_feature_importance()
+    # Save plot to file
+    avg_plot_file_path = "/Users/austinnicolas/Documents/SummerREU2024/SummerResearch2024/data_preprocessing/average_feature_importance_comparison.png"
+    plt.savefig(avg_plot_file_path)