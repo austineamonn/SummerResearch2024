1a2deffae65d46d7197cb0234bb3e77609b3ef41
austineamonn <nicolasa@grinnell.edu>
2024-07-16 15:17:40 -0400

    updated organization. added setup.py and requirements.txt files.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Intellishield',
+    version='0.9',
+    description='A Privacy Preserving Explainable Educational Recommendation System',
+    author='Austin Nicolas',
+    author_email='austineamonn@gmail.com',
+    url='https://github.com/austineamonn/SummerResearch2024',
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+)
